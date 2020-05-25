b18c5a23d8f6f97eac57abba6eb03d3e5cabc9b0
rmogusu <rmogusu123@gmail>
2020-05-26 00:07:40 +0200

    Add

--- app/src/main/java/com/moringaschool/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/RestaurantsActivity.java
@@ -12,41 +12,76 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
+    private ArrayList<Restaurant> restaurants = new ArrayList<>();
+//
+//    private String[] restaurants = new String[]{"Mi Mero Mole", "Mother's Bistro",
+//            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+//            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+//            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+//            "Chipotle", "Subway"};
+//    private String[] cuisines = new String[]{"Vegan Food", "Breakfast", "Fishs Dishs",
+//            "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican",
+//            "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican"};
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_restaurants);
+    ButterKnife.bind(this);
 
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
-    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs",
-            "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican",
-            "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurants);
-        ButterKnife.bind(this);
-        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    Intent intent = getIntent();
+    String location = intent.getStringExtra("location");
+    mLocationTextView.setText("Here are all the restaurants near: " + location);
+    getRestaurants(location);
+}
+
+    private void getRestaurants(String location){
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback(){
+
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Log.v(TAG, "In the onItemClickListener!");
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            public void onFailure(Call call, IOException e){
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                restaurants = yelpService.processResults(response);
+                RestaurantsActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] restaurantNames = new String[restaurants.size()];
+                        for(int i = 0; i<restaurantNames.length;i++){
+                            restaurantNames[i]=restaurants.get(i).getName();
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
+                        mListView.setAdapter(adapter);
+                        for (Restaurant restaurant : restaurants) {
+                            Log.d(TAG, "Name: " + restaurant.getName());
+                            Log.d(TAG, "Phone: " + restaurant.getPhone());
+                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+                        }
+                    }
+                });
             }
         });
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
-        Log.d(TAG, "In the onCreate method!");
     }
+
 }
