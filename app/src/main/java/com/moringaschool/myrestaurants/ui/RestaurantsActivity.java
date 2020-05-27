b4ab015ddb896753abbf7daa218030dd200c4449
rmogusu <rmogusu123@gmail>
2020-05-27 08:21:51 +0200

    Final commit

--- app/src/main/java/com/moringaschool/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/RestaurantsActivity.java
@@ -6,13 +6,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.myrestaurants.R;
+import com.moringaschool.myrestaurants.adapters.RestaurantListAdapter;
 import com.moringaschool.myrestaurants.models.Restaurant;
 import com.moringaschool.myrestaurants.services.YelpService;
 
@@ -29,8 +30,10 @@ import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    //@BindView(R.id.locationTextView) TextView mLocationTextView;
+    //@BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
     private ArrayList<Restaurant> restaurants = new ArrayList<>();
 
 
@@ -42,7 +45,7 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+        //mLocationTextView.setText("Here are all the restaurants near: " + location);
         getRestaurants(location);
     }
 
@@ -55,31 +58,24 @@ public class RestaurantsActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                restaurants = yelpService.processResults(response);
-                RestaurantsActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String[] restaurantNames = new String[restaurants.size()];
-                        for(int i = 0; i<restaurantNames.length;i++){
-                            restaurantNames[i]=restaurants.get(i).getName();
-                        }
-                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
-                        for (Restaurant restaurant : restaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getName());
-                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-                        }
-                    }
-                });
-            }
-        });
-    }
+                        @Override
+                        public void onResponse(Call call, Response response) {
+                            restaurants = yelpService.processResults(response);
 
-}
+                            RestaurantsActivity.this.runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    mAdapter = new RestaurantListAdapter(getApplicationContext(), restaurants);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                    RecyclerView.LayoutManager layoutManager =
+                                            new LinearLayoutManager(RestaurantsActivity.this);
+                                    mRecyclerView.setLayoutManager(layoutManager);
+                                    mRecyclerView.setHasFixedSize(true);
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+
